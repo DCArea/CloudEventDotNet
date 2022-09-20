86066a68a088b68240e775fd5f82413ceea6d183
Sun Zhongfeng <suraciii@outlook.com>
2022-09-20 10:40:48 +0800

    Make PubSubOptions.PublisherFactoris/SubscriberFactoris public

--- src/CloudEventDotNet/PubSubOptions.cs
+++ src/CloudEventDotNet/PubSubOptions.cs
@@ -1,9 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace CloudEventDotNet;
 
 public class PubSubOptions
 {
-    internal Dictionary<string, Func<IServiceProvider, ICloudEventPublisher>> PublisherFactoris { get; set; } = new();
-    internal Dictionary<string, Func<IServiceProvider, ICloudEventSubscriber>> SubscriberFactoris { get; set; } = new();
+    public Dictionary<string, Func<IServiceProvider, ICloudEventPublisher>> PublisherFactoris { get; set; } = new();
+    public Dictionary<string, Func<IServiceProvider, ICloudEventSubscriber>> SubscriberFactoris { get; set; } = new();
 }
