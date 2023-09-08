893c0afe47e3fcbde859803a9ddfe40ad7575d52
Sun Zhongfeng <suraciii@outlook.com>
2023-09-08 17:37:59 +0800

    add InstrumentAggregators

--- src/CloudEventDotNet/Telemetry/Metrics.cs
+++ src/CloudEventDotNet/Telemetry/Metrics.cs
@@ -1,5 +1,5 @@
 ﻿using System.Diagnostics.Metrics;
-using CloudEventDotNet.Diagnostics.Aggregators;
+using InstrumentAggregators;
 
 namespace CloudEventDotNet.Telemetry;
 
@@ -7,15 +7,16 @@ internal static class Metrics
 {
     internal static Meter Meter { get; } = new("DCA.CloudEvents", "0.0.1");
 
-    private static readonly CounterAggregatorGroup s_cloudEventsPublishedCounter = new(Meter, "dca_cloudevents_published");
+    private static readonly CounterAggregatorGroup s_cloudEventsPublished = new();
+    private static readonly ObservableCounter<long> s_cloudEventsPublishedCounter = s_cloudEventsPublished.CreateInstrument(Meter, "dca_cloudevents_published");
 
-    internal static readonly long[] buckets = new long[] { 5, 20, 50, 100, 200, 500, 1_000, 2_000, 5_000, 10_000, 20_000, 60_000, 2 * 60_000, 5 * 60_000, 10 * 60_000 };
+    internal static readonly long[] buckets = [5, 20, 50, 100, 200, 500, 1_000, 2_000, 5_000, 10_000, 20_000, 60_000, 2 * 60_000, 5 * 60_000, 10 * 60_000];
+    internal static readonly HistogramAggregatorGroup s_ProcessLatency = new(new(buckets));
 
-    internal static readonly HistogramAggregatorGroup s_ProcessLatency =
-        new(
-            new HistogramAggregatorOptions(buckets),
-            Meter, "dca_cloudevents_process_latency", "ms"
-        );
+    internal static readonly HistogramCounters s_ProcessLatencyCounters = s_ProcessLatency.CreateInstrument(
+        Meter,
+        "dca_cloudevents_process_latency", "ms"
+    );
 
     internal static readonly HistogramAggregatorGroup s_DeliveryTardiness =
         new(
